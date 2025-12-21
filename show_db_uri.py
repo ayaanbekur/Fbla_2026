6f75ab31f921f8001dc5b2eeb6e0f501315e00df
Ayaan Bekur <ayaanbekur@gmail.com>
2025-12-21 16:08:30 -0500

    Remove temporary/test scripts

--- show_db_uri.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-print('SQLALCHEMY_DATABASE_URI =', app.config.get('SQLALCHEMY_DATABASE_URI'))
-with app.app_context():
-    try:
-        print('Engine URL:', str(db.engine.url))
-    except Exception as e:
-        print('Error getting engine URL:', e)
